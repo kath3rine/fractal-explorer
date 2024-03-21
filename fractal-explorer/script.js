ea18073c96923ac84aae3eee67caad7b1a782bd6
katherine <111337949+kath3rine@users.noreply.github.com>
2024-03-21 00:35:17 -0400

    Delete fractal-explorer directory

--- fractal-explorer/script.js
+++
@@ -1,9 +0,0 @@
-function captionOn(text, image) {
-    document.getElementById(text).style.color = 'white';
-    document.getElementById(image).style.opacity = 0.4;
-}
-
-function captionOff(text, image) {
-    document.getElementById(text).style.color = 'transparent';
-    document.getElementById(image).style.opacity = 1;
-}
